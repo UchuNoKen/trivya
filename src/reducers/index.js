33139fc623261b7ace997a7da205b6c0a90f9ec2
UchuNoKen <xen.o.fite@gmail.com>
2019-02-08 17:56:01 -0500

    Added action and reducer for picked category.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,19 @@
-import { SET_CATEGORIES } from "../actions";
+import { combineReducers } from "redux";
+import { SET_CATEGORIES, PICK_CATEGORY } from "../actions";
 
 function categories(state = [], action) {
   switch (action.type) {
     case SET_CATEGORIES:
       return action.categories;
+    default:
+      return state;
+  }
+}
+
+function category(state = {}, action) {
+  switch (action.type) {
+    case PICK_CATEGORY:
+      return action.category;
     default:
       return state;
   }
